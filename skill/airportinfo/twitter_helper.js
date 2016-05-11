db4729ec04221b9fdb402a013db566c8b7450479
josh skeen <josh@joshskeen.com>
2016-05-11 09:57:01 -0400

    initial commit

---
+++ skill/airportinfo/twitter_helper.js
@@ -0,0 +1,25 @@
+'use strict';
+module.change_code = 1;
+var _ = require('lodash');
+var Twitter = require('twit');
+var CONSUMER_KEY = 'your consumer key here';
+var CONSUMER_SECRET = 'your consumer secret here';
+function TwitterHelper(accessToken) {
+  this.accessToken = accessToken.split(',');
+  this.client = new Twitter({
+    consumer_key: CONSUMER_KEY,
+    consumer_secret: CONSUMER_SECRET,
+    access_token: this.accessToken[0],
+    access_token_secret: this.accessToken[1]
+  });
+}
+
+TwitterHelper.prototype.postTweet = function(message) {
+  return this.client.post('statuses/update', {
+    status: message
+  }).catch(function(err) {
+      console.log('caught error', err.stack);
+    });
+};
+
+module.exports = TwitterHelper;
